86d0027a0a76aae0708d85b358af7252e12c1354
Espen Ekvang <espen.ekvang@bekk.no>
2014-02-18 14:05:17 +0100

    Added Hello.Uio

---
+++ demo/Hello.Uio/Hello.Uio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello.Uio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("____________________________________________________");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("|                 HELLO UIO!                       |");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("----------------------------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
